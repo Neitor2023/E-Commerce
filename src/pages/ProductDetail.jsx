56e3f04db8bd7b9701a02b6fd6a52c498d507a48
Romy Garcia <neitorme@gmail.com>
2023-04-06 18:14:02 -0500

    trabajando en detalle

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -18,102 +18,104 @@ const ProductDetail = () => {
   const [swBntLeft, setSwBntLeft] = useState(true)
   const [swBntRight, setSwBntRight] = useState(true)
   const [quatity, setQuatity] = useState(1)
+  const [idCategory, setIdCategory] = useState(10)
 
-
-  // dispatch = useDispatch(state.action.filterCategoriesThunk())
-  // dispatch(filterCategoriesThunk(category.id))
   const dispatch = useDispatch()
   const products = useSelector(state => state.products)
 
   useEffect(() => {
-    console.log("id ", typeof (Number(id)), " ", id)
     axios
-      .get(`https://e-commerce-api-v2.academlo.tech/api/v1/products/${Number(id)}`)
-      .then(resp => setDetail(resp.data))
+      .get(`https://e-commerce-api-v2.academlo.tech/api/v1/products/${id}`)
+      .then(resp => {
+        setDetail(resp.data)
+        setIdCategory(resp.data.categoryId)
+      })
       .catch(error => console.error(error))
-    dis()
   }, [id])
 
-  const dis = () => {
-    console.log("Category DB ", detail) // .categoryId)
-    console.log("Category ID ", detail.categoryId) // .categoryId)    
-    // console.log("Category ", detail.category?.categoryId) // .categoryId)
-    dispatch(filterCategoriesThunk(detail.categoryId))
-    console.log("Products ", products)
-  }
+  useEffect(() => {
+    dispatch(filterCategoriesThunk(idCategory))
+  }, [detail.categoryId])
 
-  // dis()
-
-  // const dispatch = useDispatch(filterCategoriesThunk(detail.categoryId))
-
-
-  //categoryId
-  // console.log("nombre ",products) 
-  //?.[0].category?.name)
   return (
     <div>
-      <div className="title">
-        <h1>{detail.title}</h1>
-        <h2>{detail.categoryId}</h2>
-      </div>
-      <div className="img_descri">
-        <div>
-          {swBntLeft &&
-            <button
-              className="img_btn"
-              onClick={() => {
-                if (btn_i > 0) {
-                  setBtn_i(btn_i - 1)
-                }
-                if (btn_i == 1) {
-                  setSwBntLeft(false)
-                }
-                if (!swBntRight) {
-                  setSwBntRight(true)
-                }
-              }}
-            >
-              <i className='bx bxs-chevron-left-circle bx-lg'></i>
-            </button>
-          }
-          <p>{detail[0]?.category.name}</p>
-          <img className="img" src={detail.images?.[btn_i].url} />
-          {swBntRight &&
-            <button
-              className="img_btn"
-              onClick={() => {
-                if (btn_i < 2) {
-                  setBtn_i(btn_i + 1)
-                }
-                if (!swBntLeft) {
-                  setSwBntLeft(true)
-                }
-                if (btn_i == 1) {
-                  setSwBntRight(false)
-                }
-              }}>
-              <i className='bx bxs-chevron-right-circle bx-lg'></i>
-            </button>
-          }
-          <div className="mini_img">
-            <img onClick={() => setBtn_i(0)} className="img_mini" src={detail.images?.[0].url} />
-            <img onClick={() => setBtn_i(1)} className="img_mini" src={detail.images?.[1].url} />
-            <img onClick={() => setBtn_i(2)} className="img_mini" src={detail.images?.[2].url} />
-          </div>
+      <div className='container_0_column'>
+        <div className="title">
+          <h1>{detail.title}</h1>
         </div>
-        <div className="container_column">
-          <div className="descri">
-            <h3>{detail.brand}</h3>
-            <p>{detail.description}</p>
-          </div>
-          <div className="container_quatity_price">
-            <div className="container_price">
-              <p>Price:</p>
-              <h5>$ {detail.price}</h5>
+        <div className='container_1_row'>
+          <div className='container2_left_column'>
+            <div className='container3_row'>
+              <div className='container4_arrow_left'>
+                {swBntLeft &&
+                  <i className='bx bxs-chevron-left-circle bx-lg'
+                    onClick={() => {
+                      if (btn_i > 0) {
+                        setBtn_i(btn_i - 1)
+                      }
+                      if (btn_i == 1) {
+                        setSwBntLeft(false)
+                      }
+                      if (!swBntRight) {
+                        setSwBntRight(true)
+                      }
+                    }}
+                  ></i>
+                }
+              </div>
+              <div className='container4_img01_center'>
+                <p>{detail[0]?.category.name}</p>
+                <img className="img" src={detail.images?.[btn_i].url} />
+              </div>
+              <div className='container4_arrow_right'>
+                {swBntRight && <i className='bx bxs-chevron-right-circle bx-lg'
+                  onClick={() => {
+                    if (btn_i < 2) {
+                      setBtn_i(btn_i + 1)
+                    }
+                    if (!swBntLeft) {
+                      setSwBntLeft(true)
+                    }
+                    if (btn_i == 1) {
+                      setSwBntRight(false)
+                    }
+                  }}
+                ></i>
+                }
+              </div>
             </div>
-            <div className="container_quatity">
-              <p>Quantity:</p>
-              <div className="quatity">
+            <div className='container5_arrow'>
+              <img onClick={() => {
+                setSwBntRight(true)
+                setSwBntLeft(false)
+                setBtn_i(0)
+              }} className="img_mini" src={detail.images?.[0].url} />
+              <img onClick={() => {
+                setSwBntRight(true)
+                setSwBntLeft(true)
+                setBtn_i(1)
+              }} className="img_mini" src={detail.images?.[1].url} />
+              <img onClick={() => {
+                setSwBntRight(false)
+                setSwBntLeft(true)
+                setBtn_i(2)
+              }} className="img_mini" src={detail.images?.[2].url} />
+            </div>
+          </div>
+          <div className='container2_right_column'>
+            <div className='container6_column'>
+              <div className='container7_brand_left'>
+                <h3>{detail.brand}</h3>
+                <p>{detail.description}</p>
+              </div>
+              <div className='price_quantity_row'>
+                <div className='price_column'>
+                  <p>Price:</p>
+                  <h5>$ {detail.price}</h5>
+                </div>
+                <div className='quantity_column'>
+                <p>Quantity:</p>
+                <div className="quatity">
                 <button onClick={() => setQuatity(quatity + 1)} className="btn_qua">
                   <h2 className="txt_qua"> + </h2>
                   {/* <i class='bx bx-message-square-add bx-sm'></i> */}
@@ -123,40 +125,30 @@ const ProductDetail = () => {
                   <h2 className="txt_qua"> - </h2>
                   {/* <i class='bx bx-message-square-minus bx-sm'></i> */}
                 </button>
+              </div>                
+                </div>
               </div>
             </div>
-          </div>
-          <div className="btn_add">
+            <div className="btn_add">
             <button className="btn_add">
               <h2 className="add_text">Add to Card  </h2><i className='bx bx-cart-add bx-md'></i>
             </button>
+          </div>            
           </div>
-        </div> {/* container_column */}
-      </div>
+        </div>
+      </div>      
       <br />
-
-      {/* <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>{products[0]?.category.id} </Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card> */}
-
+      <h2 className='discover'>Discover similar items</h2>
       <Container>
         <Row xs={1} md={2} lg={3} className='py-3'>
           {products.map(product => (
             <Col className='mb-3' key={product.id}>
-              <Card >
+              <Card
+                as={Link}
+                to={`/product/${product.id}`}
+              >
                 <Card.Img
-                onClick={()=>`/product/${product.id}`}
-                    // as={Link}
-                    // to={`/product/${product.id}`}
-                
+                  onClick={() => `/product/${product.id}`}
                   variant="top"
                   src={product.images[0].url}
                   style={{ height: 200 }} // , objectFit: "cover"
@@ -166,10 +158,6 @@ const ProductDetail = () => {
                     {product.brand}
                   </Card.Text>
                   <Card.Title>{product.title}</Card.Title>
-                  <Button
-                    as={Link}
-                    to={`/product/${product.id}`}
-                    variant="primary">Ver detalle</Button>
                 </Card.Body>
               </Card>
             </Col>
