f32f2c9fb5fa093830ef57b95fe36f34d8093b7b
Romy Garcia <neitorme@gmail.com>
2023-04-06 23:12:15 -0500

    trabajando en filter price

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,12 +10,14 @@ import { getProductsThunk, filterCategoriesThunk, filterTitleThunk } from '../st
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './Home.css'
 
 const Home = () => {
   const dispatch = useDispatch()
   const products = useSelector(state => state.products)
   const [categories, setCategories] = useState([])
   const [inputSearch, setInputSearch] = useState("")
+  const [isFromTo, setIsFromTo] = useState(false)
   useEffect(() => {
     dispatch(getProductsThunk())
 
@@ -68,6 +70,40 @@ const Home = () => {
             </InputGroup>
           </Col>
         </Row>
+        <Row>
+          <Col>
+            <Button
+              className='btn_price'
+              onClick={() => setIsFromTo(!isFromTo)}
+            >
+              <div className='btn_price'>
+                <div className='txt'>
+                  <strong>Price</strong>
+                </div>
+                <div className='bx'>
+                  {!isFromTo && <i className='bx bx-right-arrow-alt bx-sm'></i>}
+                  {isFromTo && <i className='bx bx-left-arrow-alt bx-sm'></i>}
+                </div>
+              </div>
+            </Button>
+          </Col>
+          <Col>
+            {isFromTo &&
+              <div className='input_from'>
+                <label htmlFor="">From: </label>
+                <input type="text" />
+                <label htmlFor="">To: </label>
+                <input type="text" />
+                <br />
+                <Button>Filter</Button>
+              </div>
+            }
+          </Col>
+          <Col>
+          </Col>
+          <Col>
+          </Col>
+        </Row>
         <Row xs={1} md={2} lg={3} className='py-3'>
           {products.map(product => (
             <Col className='mb-3' key={product.id}>
@@ -75,7 +111,7 @@ const Home = () => {
                 <Card.Img
                   variant="top"
                   src={product.images[0].url}
-                  style={{ height: 200}} // , objectFit: "cover"
+                  style={{ height: 200 }} // , objectFit: "cover"
                 />
                 <Card.Body>
                   <Card.Title>{product.title}</Card.Title>
