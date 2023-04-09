70ac0306cd6ec230b61e359246c56f14bc4e2dce
Romy Garcia <neitorme@gmail.com>
2023-04-09 18:01:58 -0500

    terminando from y to

--- src/store/slices/products.slice.js
+++ src/store/slices/products.slice.js
@@ -14,16 +14,23 @@ export const productsSlice = createSlice({
 })
 
 export const getProductsThunk = () => dispatch =>{
-    
+
+    dispatch(setIsLoading(true))
+
     axios.get("https://e-commerce-api-v2.academlo.tech/api/v1/products")
     .then(resp => dispatch(setProducts(resp.data)))
     .catch(error => console.error(error))
+    .finally(()=> dispatch(setIsLoading(false)))
 }
 
 export const filterCategoriesThunk = (id) => dispatch => {
+
+    dispatch(setIsLoading(true))
+
     axios.get(`https://e-commerce-api-v2.academlo.tech/api/v1/products/?categoryId=${id}`)
     .then(resp => dispatch(setProducts(resp.data)))
     .catch(error => console.error(error))
+    .finally(()=> dispatch(setIsLoading(false)))
 }
 
 export const filterTitleThunk = (value) => dispatch => {
