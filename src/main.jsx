a100e9905cb467daa5c2d14d8ced1e9da290f774
Romy Garcia <neitorme@gmail.com>
2023-04-13 13:08:12 -0500

    Finalizado

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
 import './index.css'
-import "bootswatch/dist/Spacelab/bootstrap.min.css"
+import "bootswatch/dist/spacelab/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
